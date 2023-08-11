4d291f6d8b35ece7962c437f05ff1b9d70ba121d
Marcella <marjory@hotmail.it>
2023-08-12 00:25:37 +0100

    add form

--- my-app/src/components/IngredientInput.js
+++ my-app/src/components/IngredientInput.js
@@ -1,48 +1,94 @@
-import React from 'react'
+import React from 'react';
+import { MenuItem, TextField, Button, Box, Select } from '@mui/material';
 
 function IngredientInput({ i, ingredients, handleIngredientChange }) {
 
   return (
-    <div>
-      <input
+<>
+    <Box sx={{ marginBottom: '1rem', display: 'flex', gap: '1rem' }}>
+      <TextField
         required
         type='text'
         value={ingredients[i].food}
         name='food'
+        label='Ingredient'
         placeholder='ingredient'
         onChange={(e) => handleIngredientChange(i, e)}
-        >
-        </input>
+      />
 
-        <input
+      <TextField
         required
         type='number'
         value={ingredients[i].quantity}
         name='quantity'
+        label='Quantity'
         placeholder='quantity'
+        sx={{ maxWidth: '100px' }}
         onChange={(e) => handleIngredientChange(i, e)}
-        >
-        </input>
+      />
 
-        <select
+<Select
         required
         name='measure'
         value={ingredients[i].measure}
         onChange={(e) => handleIngredientChange(i, e)}
-        >
-          <option value='unit'>unit</option>
-          <option value='can'>can</option>
-          <option value='teaspoon'>teaspoon</option>
-          <option value='tablespoon'>tablespoon</option>
-          <option value='cup'>cup</option>
-          <option value='slice'>slice</option>
-          <option value='ounce'>ounce</option>
-          <option value='pint'>pint</option>
-          <option value='quart'>quart</option>
-          <option value='gallon'>gallon</option>
-        </select>
+        style={{ flex: '1' }} // Utilizzo flex per occupare lo spazio rimanente
+      >
+        <MenuItem value='unit'>unit</MenuItem>
+        <MenuItem value='can'>can</MenuItem>
+        <MenuItem value='teaspoon'>teaspoon</MenuItem>
+        <MenuItem value='tablespoon'>tablespoon</MenuItem>
+        <MenuItem value='cup'>cup</MenuItem>
+        <MenuItem value='slice'>slice</MenuItem>
+        <MenuItem value='ounce'>ounce</MenuItem>
+        <MenuItem value='pint'>pint</MenuItem>
+        <MenuItem value='quart'>quart</MenuItem>
+        <MenuItem value='gallon'>gallon</MenuItem>
+      </Select>
+    </Box>
 
-    </div>
+{/* <Box sx={{ marginBottom: '1rem' }}>
+<TextField
+  required
+  type='text'
+  value={ingredients[i].food}
+  name='food'
+  label='Ingredient'
+  placeholder='ingredient'
+  onChange={(e) => handleIngredientChange(i, e)}
+/>
+
+<TextField
+  required
+  type='number'
+  value={ingredients[i].quantity}
+  name='quantity'
+  label='Quantity'
+  placeholder='quantity'
+  onChange={(e) => handleIngredientChange(i, e)}
+/>
+
+<TextField
+  select
+  required
+  name='measure'
+  value={ingredients[i].measure}
+  label='Measure'
+  onChange={(e) => handleIngredientChange(i, e)}
+>
+  <MenuItem value='unit'>unit</MenuItem>
+  <MenuItem value='can'>can</MenuItem>
+  <MenuItem value='teaspoon'>teaspoon</MenuItem>
+  <MenuItem value='tablespoon'>tablespoon</MenuItem>
+  <MenuItem value='cup'>cup</MenuItem>
+  <MenuItem value='slice'>slice</MenuItem>
+  <MenuItem value='ounce'>ounce</MenuItem>
+  <MenuItem value='pint'>pint</MenuItem>
+  <MenuItem value='quart'>quart</MenuItem>
+  <MenuItem value='gallon'>gallon</MenuItem>
+</TextField>
+</Box> */}
+</>
   )
 }
 
